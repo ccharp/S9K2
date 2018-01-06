b3192e21320afe7e6cc6a3ade7adc449b936d6af
Keeler Russell <keeler.russell@gmail.com>
2018-01-05 19:09:48 -0800

    chore: Rename some files

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ REMOTE="ubuntu@ec2-18-217-99-159.us-east-2.compute.amazonaws.com"
 
 rsync -avz -e "ssh -i $KEYPATH" --progress ./ $REMOTE:~/lizard/
 
-RUN_LIZARD_RUN='cd /home/ubuntu/lizard; ./run_server.sh'
+RUN_LIZARD_RUN='cd /home/ubuntu/lizard; ./run-server.sh'
 ssh -t -i $KEYPATH $REMOTE $RUN_LIZARD_RUN
 
